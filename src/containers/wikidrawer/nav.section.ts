03ccf733c577729e51f8146f090eb69a127b73b8
Pepo <pepo@uprtcl.io>
2021-01-26 12:37:42 +0100

    added reactivity to nav section new page

--- src/containers/wikidrawer/nav.section.ts
+++ src/containers/wikidrawer/nav.section.ts
@@ -1,10 +1,10 @@
-import { html, css, internalProperty,LitElement, property  } from 'lit-element';
-import { Entity, eveesConnect } from '@uprtcl/evees';
+import { html, css, internalProperty, LitElement, property } from 'lit-element';
+import { ClientEvents, Entity, eveesConnect } from '@uprtcl/evees';
 import { styles } from '@uprtcl/common-ui';
-import {Router} from '@vaadin/router'
+import { Router } from '@vaadin/router';
 
-import {GenerateSectionRoute} from '../../utils/routes.helpers'
-import {LTRouter} from '../../router'
+import { GenerateSectionRoute } from '../../utils/routes.helpers';
+import { LTRouter } from '../../router';
 import { Section } from '../types';
 export class NavSectionElement extends eveesConnect(LitElement) {
   @property()
@@ -17,19 +17,34 @@ export class NavSectionElement extends eveesConnect(LitElement) {
 
   async firstUpdated() {
     this.loading = true;
-    this.sectionData = await this.evees.getPerspectiveData(this.uref);
+    this.evees.client.events.on(ClientEvents.updated, (perspectives) =>
+      this.perspectiveUpdated(perspectives)
+    );
+    await this.load();
     this.loading = false;
   }
 
+  perspectiveUpdated(perspectives: string[]) {
+    if (perspectives.includes(this.uref)) {
+      this.load();
+    }
+  }
 
-  navigateSection(){
-    Router.go(GenerateSectionRoute(this.uref))
+  async load() {
+    this.sectionData = await this.evees.getPerspectiveData(this.uref);
+    this.requestUpdate();
+  }
+
+  navigateSection() {
+    Router.go(GenerateSectionRoute(this.uref));
   }
 
   render() {
     if (this.loading) return html`<uprtcl-loading></uprtcl-loading>`;
 
-    return html`<section class="section-heading" @click=${this.navigateSection}>${this.sectionData.object.title}</section>
+    return html`<section class="section-heading" @click=${this.navigateSection}>
+        ${this.sectionData.object.title}
+      </section>
       <uprtcl-list>
         ${this.sectionData.object.pages.map(
           (pageId) =>
@@ -45,15 +60,15 @@ export class NavSectionElement extends eveesConnect(LitElement) {
           display: flex;
           flex: 1 1 0;
           flex-direction: column;
-          margin-top:2rem;
+          margin-top: 2rem;
         }
-        .section-heading{
-          font-size:1.2rem;
+        .section-heading {
+          font-size: 1.2rem;
           text-transform: uppercase;
-          font-weight:600;
-          color:grey;
-          margin-left:2rem;
-          margin-bottom:0.2rem;
+          font-weight: 600;
+          color: grey;
+          margin-left: 2rem;
+          margin-bottom: 0.2rem;
         }
       `,
     ];

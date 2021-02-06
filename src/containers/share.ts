78a1540ac19d3b2dbf263778a1c912b98ff6e50a
Pranshu Chittora <pranshuchittora17@gmail.com>
2021-02-07 02:31:53 +0530

    feat: New share flow

--- src/containers/share.ts
+++ src/containers/share.ts
@@ -3,7 +3,9 @@ import { html, css, property, internalProperty } from 'lit-element';
 import { ConnectedElement } from '../services/connected.element';
 import { sharedStyles } from '../styles';
 import { Section } from './types';
+import ClipboardIcon from '../assets/icons/clipboard.svg';
 import { GenerateReadDocumentRoute } from '../utils/routes.helpers';
+import { LTRouter } from '../router';
 interface SectionData {
   id: string;
   data: Entity<Section>;
@@ -11,6 +13,9 @@ interface SectionData {
 export default class ShareCard extends ConnectedElement {
   @property({ type: String })
   uref: string;
+
+  @property({ type: Boolean })
+  isPagePrivate: boolean;
 
   @property({ type: String, attribute: 'from' })
   fromParentId: string;
@@ -48,6 +53,8 @@ export default class ShareCard extends ConnectedElement {
   }
 
   async load() {
+    // alert(this.isPagePrivate);
+
     const sectionIds = await this.appManager.getSections();
     this.sections = await Promise.all(
       sectionIds
@@ -75,7 +82,56 @@ export default class ShareCard extends ConnectedElement {
   render() {
     if (this.loading) return html`<uprtcl-loading></uprtcl-loading>`;
     return html`<div class="share-card-cont">
-      ${this.lastSharedPageId
+      <div class="content">
+        <div class="row">
+          <div class="heading">
+            ${this.isPagePrivate ? html`Add to blog` : html`Share to Web`}
+          </div>
+        </div>
+        <div class="row">
+          <div class="description">
+            ${this.isPagePrivate
+              ? html`Sharinng is done by adding a copy of this block somewhere
+                else.`
+              : html`Anyone with this link can view/comment on this.`}
+          </div>
+        </div>
+
+       
+        <!-- <div class="row section-row">
+          ${this.sections.map((section) => {
+          return html`<div class="add-cont">
+            <div>${section.data.object.title}:</div>
+            <uprtcl-button @click=${() => this.shareTo(section.id)}
+              >Add</uprtcl-button
+            >
+          </div>`;
+        })}
+        </div> -->
+      </div>
+      ${this.isPagePrivate
+          ? html`<div
+              @click=${() => this.shareTo(this.sections[0].id)}
+              class="add-to-blog-button"
+            >
+              Add
+            </div>`
+          : html` <div class="action-copy-cont">
+              <div class="url-cont">
+                ${window.location.origin}${GenerateReadDocumentRoute(
+                  LTRouter.Router.location.params.docId as string
+                )}
+              </div>
+              <div
+                @click=${this.copyShareURL}
+                class="copy-url-button clickable"
+              >
+                ${ClipboardIcon}
+              </div>
+            </div>`}
+        
+
+      ${this.lastSharedPageId && this.isPagePrivate
         ? html` <div class="action-copy-cont">
             <div class="url-cont">
               ${window.location.origin}${GenerateReadDocumentRoute(
@@ -83,30 +139,10 @@ export default class ShareCard extends ConnectedElement {
               )}
             </div>
             <div @click=${this.copyShareURL} class="copy-url-button clickable">
-              COPY
+              ${ClipboardIcon}
             </div>
           </div>`
         : null}
-      <div class="content">
-        <div class="row">
-          <div class="heading">Add to</div>
-        </div>
-        <div class="row">
-          <div class="description">
-            Sharing is done by adding a copy of this page somewhere else.
-          </div>
-        </div>
-        <div class="row section-row">
-          ${this.sections.map((section) => {
-            return html`<div class="add-cont">
-              <div>${section.data.object.title}:</div>
-              <uprtcl-button @click=${() => this.shareTo(section.id)}
-                >Add</uprtcl-button
-              >
-            </div>`;
-          })}
-        </div>
-      </div>
     </div>`;
   }
 
@@ -118,10 +154,10 @@ export default class ShareCard extends ConnectedElement {
           font-family: 'Poppins', sans-serif;
         }
         .share-card-cont {
-          width: 300px;
+          width: 350px;
         }
         .content {
-          padding: 0.5rem 1rem 1rem;
+          padding: 0.5rem 1rem 0rem;
         }
         .heading {
           font-size: 1.2rem;
@@ -138,6 +174,77 @@ export default class ShareCard extends ConnectedElement {
           display: flex;
           justify-content: space-around;
           align-items: center;
+        }
+        .add-to-blog-button {
+          display: inline-flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          padding: 0.5rem 1rem;
+          color: var(--white, #fff);
+          background: #4260f6;
+          box-shadow: 0px 6px 2px -4px rgba(14, 14, 44, 0.1),
+            inset 0px -1px 0px rgba(14, 14, 44, 0.4);
+          border-radius: 8px;
+          margin-top: 0.5rem;
+          margin-left:1rem;
+          margin-bottom:1rem;
+        }
+        .action-copy-cont {
+          margin: 1rem;
+          display: flex;
+
+          /* Accent */
+
+          background: #ecf1f4;
+          /* Gray 5 */
+
+          border: 1px solid #e0e0e0;
+          box-sizing: border-box;
+          /* Field/Inset */
+
+          box-shadow: inset 0px 2px 2px -1px rgba(74, 74, 104, 0.1);
+          border-radius: 0.5rem;
+          
+        }
+        .url-cont {
+          overflow-x: scroll;
+          padding: 0 0.5rem;
+          color: #4c4c5a;
+          font-size: 0.9rem;
+          display: flex;
+          justify-content: start;
+          align-items: center;
+          margin: 0 0.25rem;
+        }
+        .url-cont::-webkit-scrollbar {
+          height: 4px;
+        }
+
+        /* Track */
+        .url-cont::-webkit-scrollbar-track {
+          background: #f1f1f1;
+          height: 4px;
+        }
+
+        /* Handle */
+        .url-cont::-webkit-scrollbar-thumb {
+          background: #ccc;
+          height: 4px;
+        }
+
+        /* Handle on hover */
+        .url-cont::-webkit-scrollbar-thumb:hover {
+          background: #555;
+        }
+        .copy-url-button {
+          background: var(--white, #fff);
+          align-items: center;
+          justify-content: center;
+          display: flex;
+          padding: 0.5rem;
+          border-top-right-radius: 0.5rem;
+          border-bottom-right-radius: 0.5rem;
         }
       `,
     ];

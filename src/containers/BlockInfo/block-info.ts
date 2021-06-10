bd80e83506a42a065cf977b246c485ff3ad2ad4a
Pepo <pepo@uprtcl.io>
2021-06-10 15:53:10 +0200

    styled blcokinfo

--- src/containers/BlockInfo/block-info.ts
+++ src/containers/BlockInfo/block-info.ts
@@ -39,20 +39,22 @@ export class BlockInfoPopper extends ConnectedElement {
     return html`
       ${
         this.forks && this.forks.length > 0
-          ? html`<uprtcl-popper skinny position="bottom-left">
-              <uprtcl-icon-button icon="fork" slot="icon" button skinny
-                >${this.forks.length}
-              </uprtcl-icon-button>
-              <uprtcl-card
-                >Fork ids:
-                ${this.forks.map(
-                  (e) => html`<li>${e.childId} on ${e.parentId}</li>`
-                )}
-              </uprtcl-card>
-            </uprtcl-popper>`
+          ? html`<div class="forks">
+              <div class="number">8</div>
+              <uprtcl-popper skinny position="bottom-left">
+                <uprtcl-icon-button icon="fork" slot="icon" button skinny>
+                </uprtcl-icon-button>
+                <uprtcl-card
+                  >Fork ids:
+                  ${this.forks.map(
+                    (e) => html`<li>${e.childId} on ${e.parentId}</li>`
+                  )}
+                </uprtcl-card>
+              </uprtcl-popper>
+            </div> `
           : ``
       }
-      <uprtcl-popper skinny icon="menu" position="bottom-left">
+      <uprtcl-popper skinny icon="two_dots" position="bottom-left">
         <uprtcl-card><uprtcl-button @click=${() =>
           this.forkBlock()}>Fork</uprtcl-button></uprtcl-button></uprtcl-card>
       </uprtcl-popper>
@@ -64,8 +66,23 @@ export class BlockInfoPopper extends ConnectedElement {
       sharedStyles,
       css`
         :host {
-          padding: 0 5px;
+          position: relative;
+        }
+
+        .number {
+          font-family: 'Inter';
+          font-weight: 500;
+          font-size: 1rem;
+        }
+
+        .forks {
+          position: absolute;
+          margin-left: -46styledpx;
+          --svg-fill: #7d7d9250;
+          --svg-height: 18px;
+          color: #7d7d9250;
           display: flex;
+          align-items: center;
         }
       `,
     ];
